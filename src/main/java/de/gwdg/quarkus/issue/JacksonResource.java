b5b1420f2a0f231efaf2444a004368dd99755351
Marcel Hellkamp <marc@gsites.de>
2021-01-21 16:02:40 +0100

    Whitespace

--- src/main/java/de/gwdg/quarkus/issue/JacksonResource.java
+++ src/main/java/de/gwdg/quarkus/issue/JacksonResource.java
@@ -8,16 +8,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.MultivaluedMap;
 
-@ApplicationScoped
 @Path("/test")
+@ApplicationScoped
 public class JacksonResource {
 
 	@POST
-    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 	public String test(
 			@FormParam("foo") String foo,
 			MultivaluedMap<String, String> form) {
 		return "foo=" + foo + " form=" + form.toString();
-    }
+	}
 
 }
